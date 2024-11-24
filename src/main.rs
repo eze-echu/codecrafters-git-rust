e5b94047f6444621741d6ad63adb9dec06034f5e
Ezequiel Oyhambure <ezequiel@oyhambure.com>
2024-11-24 11:48:42 -0300

    added cat-file functionality

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use flate2::read::*;
 #[allow(unused_imports)]
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +12,34 @@ fn main() {
 
     // Uncomment this block to pass the first stage
     let args: Vec<String> = env::args().collect();
-    if args[1] == "init" {
-        fs::create_dir(".git").unwrap();
-        fs::create_dir(".git/objects").unwrap();
-        fs::create_dir(".git/refs").unwrap();
-        fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-        println!("Initialized git directory")
-    } else {
-        println!("unknown command: {}", args[1])
+    match args[1].as_str() {
+        "init" => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git directory");
+        }
+        "cat-file" => {
+            let hash = &args[2];
+            let path = Path::new(".git/objects")
+                .join(hash[..2].chars().as_str())
+                .join(hash[2..].chars().as_str());
+            let zlib_compressed_string = fs::read_to_string(&path).unwrap_or_else(|e| {
+                panic!(
+                    "Unable to read file: {}\n Error: {e}",
+                    path.to_string_lossy()
+                )
+            });
+            let mut decoder = ZlibDecoder::new(zlib_compressed_string.as_bytes());
+            let mut uncompressed_string: String = String::new();
+            decoder
+                .read_to_string(&mut uncompressed_string)
+                .unwrap_or_else(|e| panic!("Failed to decode ZLib file:\n {e}"));
+            print!("{}", uncompressed_string);
+        }
+        _ => {
+            println!("unknown command: {}", args[1]);
+        }
     }
 }
